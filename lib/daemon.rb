aeebdb6e5880ddf6f2c91dc73b97ffd5e05a3d5d
Magnus Rosenquist <mange.rosenquist@gmail.com>
2014-09-11 20:56:10 +0000

    Publish data to redis channel upon receiving it, faking some sort of job

--- lib/daemon.rb
+++ lib/daemon.rb
@@ -2,9 +2,13 @@ module Gorilla
 
   class Daemon 
     @queue = :aggregate
-    
+    CHANNEL = "gorilla-pipeline"
+
     def initialize
       puts "Initialized worker"
+      redis_url = ENV["REDISCLOUD_URL"]
+      uri = URI.parse(redis_url)
+      @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     end
     
     def self.perform(json)
@@ -13,6 +17,11 @@ module Gorilla
   
     def perform_aggregation(json)
       flush "Running background job with json-data: #{json}"
+      data = JSON.parse(json, :symbolize_names => true)
+      flush data.inspect
+      payload = {:handle => "aggregator", :text => data[:text]}.to_json
+      @redis.publish(CHANNEL, payload)
+      flush "published result"
     end
     
     private 
